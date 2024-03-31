b7e7c7972e194536da9a123dd7b0f55d3c48e9e2
Andrew Gerez <andrewgerez18@gmail.com>
2024-03-30 23:53:52 -0300

    feat: use element enum

--- cypress/e2e/unlogged/demonstration.cy.ts
+++ cypress/e2e/unlogged/demonstration.cy.ts
@@ -1,5 +1,5 @@
 import { env } from '@/env'
-import { CyKeyTypeEnum } from '@/support/utils/cy-types'
+import { CyElementEnum, CyKeyTypeEnum } from '@/support/utils/cy-types'
 
 describe('TEST: Demonstration spec', () => {
   it('should be able to navigate from the first page to the login page', async () => {
@@ -7,13 +7,18 @@ describe('TEST: Demonstration spec', () => {
 
     cy.url().should('eq', env.E2E_ENVIRONMENT_URL)
 
-    cy.get('h1').contains('Os melhores conteúdos em um único lugar')
+    const displayText = 'Os melhores conteúdos em um único lugar'
+
+    cy.get(CyElementEnum.H1).contains(displayText)
       .should('have.length', 1)
 
     cy.wait(1800)
 
-    cy.get('button').focus().type(CyKeyTypeEnum.Enter)
+    cy.get(CyElementEnum.Button)
+      .focus()
+      .type(CyKeyTypeEnum.Enter)
 
-    cy.url().should('eq', env.E2E_ENVIRONMENT_URL + env.E2E_AUTH_ROUTE)
+    cy.url()
+      .should('eq', env.E2E_ENVIRONMENT_URL + env.E2E_AUTH_ROUTE)
   })
 })
--- cypress/support/utils/cy-types/index.ts
+++ cypress/support/utils/cy-types/index.ts
@@ -1,3 +1,8 @@
+export enum CyElementEnum {
+  Button = 'button',
+  H1 = 'h1',
+}
+
 export enum CyKeyTypeEnum {
   Enter = '{enter}',
 }
